ed62043888c3abebec4b4739b88277b3b56dca1f
Viktroia <vikmolotova@gmaill.com>
2024-02-10 12:55:27 +0500

    first commit

---
+++ pract7.cpp
@@ -0,0 +1,28 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include <stdio.h>
+#include <locale.h>  
+
+void ShowAllNegativeNumbers(FILE* file) {
+	int number=0;
+	if (fscanf(file, "%d", &number)==1) {
+		if (number < 0)
+			std::cout << number<<"\n";
+		ShowAllNegativeNumbers(file);
+	}
+	else {
+		return;
+	}
+}
+
+int main()
+{
+	FILE* file;
+	if ((file = fopen("numbers.txt", "r")) == NULL) {
+		printf("Файл невозможно создать или открыть\n");
+		return 1;
+	}
+	ShowAllNegativeNumbers(file);
+}
+
+
